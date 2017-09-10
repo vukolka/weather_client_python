96e08cfb541d8827214168eae91c93c8ea66bf75
kolyaRage <vukolov.kolya@gmail.com>
2017-09-10 05:57:18 +0300

    so now it knows where do you live. in case to work it needs geocoder to be intalled on machine http://geocoder.readthedocs.io/

--- main_yahoo.py
+++ main_yahoo.py
@@ -1,9 +1,14 @@
 import yahooAPI_lib
-import sys
-city = sys.argv[1]
-state = sys.argv[2]
-print (city)
-print (state)
-Kiev = yahooAPI_lib.weather(city, state)
+import geocoder
+import http.client
+#gettin ip adress
+conn = http.client.HTTPConnection("ip.42.pl")
+conn.request("GET", "/raw")
+respond = conn.getresponse()
+data = respond.read()
+conn.close()
+
+g = geocoder.freegeoip(data.decode())
+Kiev = yahooAPI_lib.weather(g.json['city'], g.json["raw"]['country_code'])
 temp = Kiev.get_current_temp()
 print (temp)
